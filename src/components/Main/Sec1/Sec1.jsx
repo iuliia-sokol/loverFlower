85c1f4c5a74c830faad78b2074e587864de62bb9
Iuliia Sokol <avare779@gmail.com>
2023-03-02 16:24:23 +0200

    sec2 edited

--- src/components/Main/Sec1/Sec1.jsx
+++ src/components/Main/Sec1/Sec1.jsx
@@ -1,7 +1,15 @@
 import { BgText } from 'components/BgText/BgText';
 import { Container } from 'components/Container/Container';
 import { Section } from 'components/SectionWrapper/SectionWrapper';
-import { BgTextWrapper, BgWrapper, Gradient } from './Sec1.styled';
+import {
+  BgTextWrapper,
+  BgWrapper,
+  CatalogLink,
+  Gradient,
+  TextBlock,
+  TextBlockHeader,
+  TextBlocksWrapper,
+} from './Sec1.styled';
 import BgGradient from '../../../images/main/dark-gradient-min.png';
 import { SectionHeader } from 'components/SectionHeader/SectionHeader';
 
@@ -13,11 +21,52 @@ export const Section1 = () => {
         <BgTextWrapper>
           <BgText text="букети" />
           <BgText text="квіти" />
-          <BgText text="подарунки" />
+          <BgText text="додатково" />
         </BgTextWrapper>
       </BgWrapper>
       <Container>
         <SectionHeader text="каталог" />
+        <TextBlocksWrapper>
+          <TextBlock>
+            <span>
+              У нас найбільший вибір квітів, букетів, листівок та подарунків. Ми
+              завжди допоможемо вам підібрати букет для вашої події, наш
+              менеджер вас проконсультує та допоможе визначитися з вибором.
+            </span>
+            <span>Ознайомтеся з нашими розділами каталогу</span>
+          </TextBlock>
+          <TextBlock>
+            <TextBlockHeader>готові букети з сухоцвітів</TextBlockHeader>
+            <ul>
+              <li>букети</li>
+              <li>для інтер'єру</li>
+              <li>композиції</li>
+            </ul>
+            <CatalogLink>дивитись каталог</CatalogLink>
+          </TextBlock>
+          <TextBlock>
+            <TextBlockHeader>квіти</TextBlockHeader>
+            <ul>
+              <li>збірні букети</li>
+              <li>монобукети</li>
+              <li>композиції із квітів</li>
+              <li>троянди</li>
+              <li>весільні композиції</li>
+            </ul>
+            <CatalogLink>дивитись каталог</CatalogLink>
+          </TextBlock>
+          <TextBlock>
+            <TextBlockHeader>додатково</TextBlockHeader>
+            <ul>
+              <li>кульки</li>
+              <li>іграшки</li>
+              <li>листівки</li>
+              <li>обгортка</li>
+            </ul>
+
+            <CatalogLink>дивитись каталог</CatalogLink>
+          </TextBlock>
+        </TextBlocksWrapper>
       </Container>
     </Section>
   );
--- src/components/Main/Sec1/Sec1.styled.js
+++ src/components/Main/Sec1/Sec1.styled.js
@@ -64,11 +64,84 @@ export const BgTextWrapper = styled.div`
   width: 150%;
   z-index: -3;
 
-  & p:first-child {
+  & p:first-child,
+  & p:last-child {
     text-align: center;
   }
+`;
 
-  & p:last-child {
-    text-align: end;
+export const TextBlocksWrapper = styled.div`
+  display: flex;
+  margin-top: 100px;
+  flex-wrap: wrap;
+  gap: 30px;
+  width: 100%;
+
+  & div:first-child {
+    width: calc(55% - 60px);
+    transform: translateY(-100px);
+    z-index: -1;
   }
+
+  & div:nth-child(2) {
+    width: calc(35% - 60px);
+
+    & ul {
+      padding-left: 12px;
+      list-style: circle;
+      text-transform: uppercase;
+    }
+  }
+
+  & div:nth-child(3) {
+    width: calc(55% - 60px);
+    transform: translateY(-100px);
+
+    & ul {
+      padding-left: 12px;
+      list-style: circle;
+      text-transform: uppercase;
+    }
+  }
+
+  & div:last-child {
+    width: calc(35% - 60px);
+
+    & ul {
+      padding-left: 12px;
+      list-style: circle;
+      text-transform: uppercase;
+    }
+  }
+`;
+
+export const TextBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  padding: 30px 50px;
+  font-family: ${p => p.theme.fonts.main};
+  font-size: 20px;
+  font-weight: ${p => p.theme.fontWeights[0]};
+  line-height: ${p => p.theme.lineHeights.content};
+  letter-spacing: ${p => p.theme.letterSpacings.content};
+  color: ${p => p.theme.colors.mainLight};
+  background: rgba(0, 0, 0, 0.2);
+  backdrop-filter: blur(10px);
+  border-radius: ${p => p.theme.radii.blocks};
+`;
+
+export const TextBlockHeader = styled.h4`
+  font-size: 20px;
+  text-transform: uppercase;
+  font-weight: ${p => p.theme.fontWeights[4]};
+  line-height: ${p => p.theme.lineHeights.content};
+  letter-spacing: ${p => p.theme.letterSpacings.content};
+  color: ${p => p.theme.colors.accentGreen};
+`;
+
+export const CatalogLink = styled.a`
+  text-decoration: underline;
+  text-align: end;
+  color: ${p => p.theme.colors.accentPink};
 `;
